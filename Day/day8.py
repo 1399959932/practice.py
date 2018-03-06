007a713507670cc3d2141dfa21e35ae9832995ee
Aaron <1399959932@qq.com>
2018-03-06 18:50:06 +0800

    3.6补

---
+++ Day/day8.py
@@ -0,0 +1,15 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def hello():
+	print('hello, world')
+
+	
+>>> temp = hello()
+hello, world
+>>> hello()
+hello, world
+>>> print(temp)
+None
+>>> type(temp)
+<class 'NoneType'>
+>>> 
